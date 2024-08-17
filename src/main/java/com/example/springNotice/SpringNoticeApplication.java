dff936c4c7977c06bc9050fe360902b0364288a4
pure77 <72856694+pure77@users.noreply.github.com>
2024-08-17 22:42:24 +0900

    스프링 게시판 프로젝트 깃허브 셋팅

---
+++ src/main/java/com/example/springNotice/SpringNoticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springNotice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNoticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNoticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springNotice/SpringNoticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springNotice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNoticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
